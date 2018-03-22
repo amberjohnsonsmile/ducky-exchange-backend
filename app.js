8cb2afcc8ea235d211e5490a666bcf62513eff19
Amber Johnson <amber.johnson.smile@gmail.com>
2018-03-21 21:57:25 -0600

    Add async await to manage multiple axios requests

--- app.js
+++ app.js
@@ -31,77 +31,96 @@ app.post('/', (request, response) => {
 let body = {};
 
 // Bittrex DASH
-axios
-  .get('https://bittrex.com/api/v1.1/public/getmarketsummary?market=btc-dash')
-  .then(response => {
-    body.bittrex_DASH = response.data.result[0].Last;
-  })
-  .catch(console.error);
+function bittrex() {
+  axios
+    .get('https://bittrex.com/api/v1.1/public/getmarketsummary?market=btc-dash')
+    .then(response => {
+      body.bittrex_DASH = response.data.result[0].Last;
+    })
+    .catch(console.error);
+}
+
+// Still researching async await syntax
+async function getRates() {
+  await bittrex();
+  console.log(body)
+}
+
+getRates();
 
 // Bittrex LTC
-axios
-  .get('https://bittrex.com/api/v1.1/public/getmarketsummary?market=btc-ltc')
-  .then(response => {
-    body.bittrex_LTC = response.data.result[0].Last;
-  })
-  .catch(console.error);
+// axios
+//   .get('https://bittrex.com/api/v1.1/public/getmarketsummary?market=btc-ltc')
+//   .then(response => {
+//     body.bittrex_LTC = response.data.result[0].Last;
+//   })
+//   .catch(console.error);
 
-// Bittrex ETH
-axios
-  .get('https://bittrex.com/api/v1.1/public/getmarketsummary?market=btc-eth')
-  .then(response => {
-    body.bittrex_ETH = response.data.result[0].Last;
-  })
-  .catch(console.error);
+// // Bittrex ETH
+// axios
+//   .get('https://bittrex.com/api/v1.1/public/getmarketsummary?market=btc-eth')
+//   .then(response => {
+//     body.bittrex_ETH = response.data.result[0].Last;
+//   })
+//   .catch(console.error);
 
-// CoinCap DASH
-axios
-  .get('http://coincap.io/page/DASH')
-  .then(response => {
-    body.coincap_DASH = response.data.price_btc;
-    console.log(body);
-  })
-  .catch(console.error);
+// // CoinCap DASH
+// axios
+//   .get('http://coincap.io/page/DASH')
+//   .then(response => {
+//     body.coincap_DASH = response.data.price_btc;
+//     console.log(body);
+//   })
+//   .catch(console.error);
 
-// CoinCap LTC
-axios
-  .get('http://coincap.io/page/LTC')
-  .then(response => {
-    body.coincap_LTC = response.data.price_btc;
-    console.log(body);
-  })
-  .catch(console.error);
+// // CoinCap LTC
+// axios
+//   .get('http://coincap.io/page/LTC')
+//   .then(response => {
+//     body.coincap_LTC = response.data.price_btc;
+//     console.log(body);
+//   })
+//   .catch(console.error);
 
-// CoinCap ETH
-axios
-  .get('http://coincap.io/page/ETH')
-  .then(response => {
-    body.coincap_ETH = response.data.price_btc;
-    console.log(body);
-  })
-  .catch(console.error);
+// // CoinCap ETH
+// axios
+//   .get('http://coincap.io/page/ETH')
+//   .then(response => {
+//     body.coincap_ETH = response.data.price_btc;
+//     console.log(body);
+//   })
+//   .catch(console.error);
 
-// Kraken API
-axios
-  .get('https://api.kraken.com/0/public/Ticker?pair=dashxbt,xltcxxbt,xethxxbt')
-  .then(response => {
-    body.kraken_DASH = parseFloat(response.data.result.DASHXBT.p[0]);
-    body.kraken_ETH = parseFloat(response.data.result.XETHXXBT.p[0]);
-    body.kraken_LTC = parseFloat(response.data.result.XLTCXXBT.p[0]);
-  })
-  .catch(console.error);
+// // Kraken API
+// axios
+//   .get('https://api.kraken.com/0/public/Ticker?pair=dashxbt,xltcxxbt,xethxxbt')
+//   .then(response => {
+//     body.kraken_DASH = parseFloat(response.data.result.DASHXBT.p[0]);
+//     body.kraken_ETH = parseFloat(response.data.result.XETHXXBT.p[0]);
+//     body.kraken_LTC = parseFloat(response.data.result.XLTCXXBT.p[0]);
+//   })
+//   .catch(console.error);
 
-// Poloniex API
-axios
-  .get('https://poloniex.com/public?command=returnTicker')
-  .then(response => {
-    body.poloniex_DASH = parseFloat(response.data.BTC_DASH.last);
-    body.poloniex_ETH = parseFloat(response.data.BTC_ETH.last);
-    body.poloniex_LTC = parseFloat(response.data.BTC_LTC.last);
-  })
-  .catch(console.error);
+// // Poloniex API
+// axios
+//   .get('https://poloniex.com/public?command=returnTicker')
+//   .then(response => {
+//     body.poloniex_DASH = parseFloat(response.data.BTC_DASH.last);
+//     body.poloniex_ETH = parseFloat(response.data.BTC_ETH.last);
+//     body.poloniex_LTC = parseFloat(response.data.BTC_LTC.last);
+//   })
+//   .catch(console.error);
 
 // axios.post to my server to add data?
+// Need to wait until all post requests are complete... async await? How do I do that?
+
+// async function getABC() {
+//   let A = await getValueA(); // getValueA takes 2 second to finish
+//   let B = await getValueB(); // getValueB takes 4 second to finish
+//   let C = await getValueC(); // getValueC takes 3 second to finish
+
+//   return A*B*C;
+// }
 
 app.use((request, response) => {
   response.sendStatus(404);
