1d711fc167c6783c2a344ec480beaceb3d480339
Amber Johnson <amber.johnson.smile@gmail.com>
2018-03-22 09:49:29 -0600

    Get rates and post to DB

--- app.js
+++ app.js
@@ -4,26 +4,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const axios = require('axios');
 const queries = require('./queries');
-// const knex = require('./database-connection');
-
-// Need to bring knex in to insert, or could post
-
-// knex('exchange').insert([
-//   {
-//     bittrex_DASH: 0.3,
-//     bittrex_ETH: 0.3,
-//     bittrex_LTC: 0.3,
-//     coincap_DASH: 0.3,
-//     coincap_ETH: 0.3,
-//     coincap_LTC: 0.3,
-//     kraken_DASH: 0.3,
-//     kraken_ETH: 0.3,
-//     kraken_LTC: 0.3,
-//     poloniex_DASH: 0.3,
-//     poloniex_ETH: 0.3,
-//     poloniex_LTC: 0.3
-//   }
-// ]);
+const postURL = 'http://localhost:3000';
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -48,123 +29,160 @@ app.post('/', (request, response) => {
 
 // GET request for only latest item?
 
+// Repeat get requests every 5 minutes
+// setInterval(() => {
+//   // your code goes here...
+// }, 500);
+
 let body = {};
 let counter = 0;
 
+function getRates() {
+  bittrexDASH();
+  bittrexETH();
+  bittrexLTC();
+  coincapDASH();
+  coincapETH();
+  coincapLTC();
+  kraken();
+  poloniex();
+}
+
+getRates();
+
+function addRates() {
+  axios
+    .post(postURL, body)
+    .then(console.log)
+    .catch(console.error);
+}
+
 // Bittrex DASH
-bittrexDASH() {
-axios
-  .get('https://bittrex.com/api/v1.1/public/getmarketsummary?market=btc-dash')
-  .then(response => {
-    body.bittrex_DASH = response.data.result[0].Last;
-    counter++;
-    if (counter === 8) {
-      console.log(body);
-    }
-  })
-  .catch(console.error);
+
+function bittrexDASH() {
+  axios
+    .get('https://bittrex.com/api/v1.1/public/getmarketsummary?market=btc-dash')
+    .then(response => {
+      body.bittrex_DASH = response.data.result[0].Last;
+      counter++;
+      if (counter === 8) {
+        console.log(body);
+        addRates();
+      }
+    })
+    .catch(console.error);
 }
 
 // Bittrex ETH
-bittrexETH() {
-axios
-  .get('https://bittrex.com/api/v1.1/public/getmarketsummary?market=btc-eth')
-  .then(response => {
-    body.bittrex_ETH = response.data.result[0].Last;
-    counter++;
-    if (counter === 8) {
-      console.log(body);
-    }
-  })
-  .catch(console.error);
+function bittrexETH() {
+  axios
+    .get('https://bittrex.com/api/v1.1/public/getmarketsummary?market=btc-eth')
+    .then(response => {
+      body.bittrex_ETH = response.data.result[0].Last;
+      counter++;
+      if (counter === 8) {
+        console.log(body);
+        addRates();
+      }
+    })
+    .catch(console.error);
 }
 
 // Bittrex LTC
-bittrexLTCH() {
-axios
-  .get('https://bittrex.com/api/v1.1/public/getmarketsummary?market=btc-ltc')
-  .then(response => {
-    body.bittrex_LTC = response.data.result[0].Last;
-    counter++;
-    if (counter === 8) {
-      console.log(body);
-    }
-  })
-  .catch(console.error);
+function bittrexLTC() {
+  axios
+    .get('https://bittrex.com/api/v1.1/public/getmarketsummary?market=btc-ltc')
+    .then(response => {
+      body.bittrex_LTC = response.data.result[0].Last;
+      counter++;
+      if (counter === 8) {
+        console.log(body);
+        addRates();
+      }
+    })
+    .catch(console.error);
 }
 
 // CoinCap DASH
-coincapDASH() {
-axios
-  .get('http://coincap.io/page/DASH')
-  .then(response => {
-    body.coincap_DASH = response.data.price_btc;
-    counter++;
-    if (counter === 8) {
-      console.log(body);
-    }
-  })
-  .catch(console.error);
+function coincapDASH() {
+  axios
+    .get('http://coincap.io/page/DASH')
+    .then(response => {
+      body.coincap_DASH = response.data.price_btc;
+      counter++;
+      if (counter === 8) {
+        console.log(body);
+        addRates();
+      }
+    })
+    .catch(console.error);
+}
+
+// CoinCap ETH
+function coincapETH() {
+  axios
+    .get('http://coincap.io/page/ETH')
+    .then(response => {
+      body.coincap_ETH = response.data.price_btc;
+      counter++;
+      if (counter === 8) {
+        console.log(body);
+        addRates();
+      }
+    })
+    .catch(console.error);
 }
 
 // CoinCap LTC
-axios
-  .get('http://coincap.io/page/LTC')
-  .then(response => {
-    body.coincap_LTC = response.data.price_btc;
-    counter++;
-    if (counter === 8) {
-      console.log(body);
-    }
-  })
-  .catch(console.error);
-
-// CoinCap ETH
-axios
-  .get('http://coincap.io/page/ETH')
-  .then(response => {
-    body.coincap_ETH = response.data.price_btc;
-    counter++;
-    if (counter === 8) {
-      console.log(body);
-    }
-  })
-  .catch(console.error);
+function coincapLTC() {
+  axios
+    .get('http://coincap.io/page/LTC')
+    .then(response => {
+      body.coincap_LTC = response.data.price_btc;
+      counter++;
+      if (counter === 8) {
+        console.log(body);
+        addRates();
+      }
+    })
+    .catch(console.error);
+}
 
 // Kraken API
-axios
-  .get('https://api.kraken.com/0/public/Ticker?pair=dashxbt,xltcxxbt,xethxxbt')
-  .then(response => {
-    body.kraken_DASH = parseFloat(response.data.result.DASHXBT.p[0]);
-    body.kraken_ETH = parseFloat(response.data.result.XETHXXBT.p[0]);
-    body.kraken_LTC = parseFloat(response.data.result.XLTCXXBT.p[0]);
-    counter++;
-    if (counter === 8) {
-      console.log(body);
-    }
-  })
-  .catch(console.error);
+function kraken() {
+  axios
+    .get(
+      'https://api.kraken.com/0/public/Ticker?pair=dashxbt,xltcxxbt,xethxxbt'
+    )
+    .then(response => {
+      body.kraken_DASH = parseFloat(response.data.result.DASHXBT.p[0]);
+      body.kraken_ETH = parseFloat(response.data.result.XETHXXBT.p[0]);
+      body.kraken_LTC = parseFloat(response.data.result.XLTCXXBT.p[0]);
+      counter++;
+      if (counter === 8) {
+        console.log(body);
+        addRates();
+      }
+    })
+    .catch(console.error);
+}
 
 // Poloniex API
-axios
-  .get('https://poloniex.com/public?command=returnTicker')
-  .then(response => {
-    body.poloniex_DASH = parseFloat(response.data.BTC_DASH.last);
-    body.poloniex_ETH = parseFloat(response.data.BTC_ETH.last);
-    body.poloniex_LTC = parseFloat(response.data.BTC_LTC.last);
-    counter++;
-    if (counter === 8) {
-      console.log(body);
-    }
-  })
-  .catch(console.error);
-
-// Still researching async await syntax
-// async function getRates() {
-//   await bittrexDASH();
-//   console.log(body);
-// }
-// getRates();
+function poloniex() {
+  axios
+    .get('https://poloniex.com/public?command=returnTicker')
+    .then(response => {
+      body.poloniex_DASH = parseFloat(response.data.BTC_DASH.last);
+      body.poloniex_ETH = parseFloat(response.data.BTC_ETH.last);
+      body.poloniex_LTC = parseFloat(response.data.BTC_LTC.last);
+      counter++;
+      if (counter === 8) {
+        console.log(body);
+        addRates();
+      }
+    })
+    .catch(console.error);
+}
 
 app.use((request, response) => {
   response.sendStatus(404);
